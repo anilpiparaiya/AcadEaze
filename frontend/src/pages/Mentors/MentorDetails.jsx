4d1a0eee204160e3aef7881308125d1e360b4ed2
Anil Piparaiya <anilkumarpiparaiya@gmail.com>
2023-12-11 12:02:15 +0530

    first commit

---
+++ frontend/src/pages/Mentors/MentorDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MentorDetails = () => {
+  return (
+    <div>MentorDetails</div>
+  )
+}
+
+export default MentorDetails
---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,38 @@
+import Home from "../pages/Home"
+import About from "../pages/About"
+
+import Classes from "../pages/Classes"
+import Pages from "../pages/Pages"
+import Blog from "../pages/Blog"
+import Services from "../pages/Services"
+import Login from "../pages/Login"
+import Signup from "../pages/Signup"
+import Contact from "../pages/Contact"
+import Mentors from "../pages/Mentors/Mentors"
+import MentorDetails from "../pages/Mentors/MentorDetails"
+
+import {Routes, Route} from "react-router-dom"
+import Team from "../pages/Team"
+
+const Routers = () => {
+  return <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/classes" element={<Classes />} />
+      <Route path="/pages" element={<Pages />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/mentors" element={<Mentors />} />
+      <Route path="/MentorDetails" element={<MentorDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+      
+
+
+    </Routes>
+  
+};
+
+export default Routers
