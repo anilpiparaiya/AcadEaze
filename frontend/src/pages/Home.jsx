55de7794bbf0f5846a829b6f6d7f26d3e27096eb
Anil Piparaiya <anilkumarpiparaiya@gmail.com>
2023-12-16 21:11:44 +0530

    add hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
-
+import React from "react";
+import heroImg01 from "../assets/images/hero-img01.jpg"
+import heroImg02 from "../assets/images/hero-img02.jpg"
+import heroImg03 from "../assets/images/hero-img03.jpg"
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      {/* Hero Section */}
+      <section className="hero__section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            {/* Hero content */}
 
-export default Home
+            <div>
+              <div className="lg:w-[570px]">
+                <h1 className="lg:w-[570px] leading-[460px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                  We help students to ace their study
+                </h1>
+
+                <p className="text__para">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                  doloribus eveniet repellendus! Velit deleniti totam
+                  reiciendis, alias voluptates in culpa impedit libero commodi
+                  quos officiis harum fugiat reprehenderit ab veritatis!
+                </p>
+
+                <button className="btn">Request an Appointment</button>
+              </div>
+
+              <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                <div>
+                  <h2 className="text-[36px] leading-[50px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    30+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Years of Experience</p>
+                </div>
+
+
+                <div>
+                  <h2 className="text-[36px] leading-[50px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    15+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Institute Location</p>
+                </div>
+
+
+                <div>
+                  <h2 className="text-[36px] leading-[50px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    100%
+                  </h2>
+                  <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Student Satisfaction</p>
+                </div>
+
+
+              </div>
+            </div>
+
+            {/*  Hero content*/}
+            <div className="flex gap-[30px] justify-end">
+              <div>
+                <img src={heroImg01} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <img src={heroImg02} alt="" className="w-full mb-[30px]"/>
+                <img src={heroImg03} alt="" className="w-full"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
