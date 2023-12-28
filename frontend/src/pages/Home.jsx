f1b9c3a490f815c743b8b7917c1efc4cf7120102
Anil Piparaiya <anilkumarpiparaiya@gmail.com>
2023-12-28 17:51:13 +0530

    some changes in about section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,8 +6,8 @@ import icon01 from "../assets/images/icon01.jpg";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
 import featureImg from "../assets/images/feature-img.png";
-import videoIcon from '../assets/images/video-icon.png'
-import avatarIcon from "../assets/images/avatar-icon.png"
+import videoIcon from "../assets/images/video-icon.png";
+import avatarIcon from "../assets/images/avatar-icon.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
@@ -29,10 +29,19 @@ const Home = () => {
                 </h1>
 
                 <p className="text__para">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-                  doloribus eveniet repellendus! Velit deleniti totam
-                  reiciendis, alias voluptates in culpa impedit libero commodi
-                  quos officiis harum fugiat reprehenderit ab veritatis!
+                  Welcome to our Mentors Appointment website, where guidance
+                  meets convenience. Our platform is designed to connect you
+                  with experienced and dedicated mentors who can provide
+                  valuable insights and support across various fields. Whether
+                  you're navigating a career change, seeking advice on personal
+                  development, or aiming to enhance specific skills, our mentors
+                  are here to help you chart your course to success. With
+                  user-friendly features, you can easily browse through a
+                  diverse pool of mentors, view their profiles, and schedule
+                  appointments at your convenience. We believe in the power of
+                  mentorship to inspire, guide, and propel individuals towards
+                  their goals. Join us on this transformative journey and take
+                  the next step towards unlocking your full potential.
                 </p>
 
                 <button className="btn">Request an Appointment</button>
@@ -232,38 +241,52 @@ const Home = () => {
           <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
             <img src={featureImg} className="w-3/4" alt="" />
 
-            <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] 
+            <div
+              className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] 
             md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] 
-            rounded-[10px]">
+            rounded-[10px]"
+            >
               <div className="flex items-center justify-between">
                 <div className="flex items-center gap-[6px] lg:gap-3">
-                  <p className="text-[10px] leading-[10px] lg:text-[14px] lg: leading-5 text-headingColor 
-                  font-[600]">
+                  <p
+                    className="text-[10px] leading-[10px] lg:text-[14px] lg: leading-5 text-headingColor 
+                  font-[600]"
+                  >
                     Tue, 24
                   </p>
 
-                  <p className="text-[10px] leading-[10px] lg:text-[14px] lg: leading-5 text-textColor 
-                  font-[400]">
+                  <p
+                    className="text-[10px] leading-[10px] lg:text-[14px] lg: leading-5 text-textColor 
+                  font-[400]"
+                  >
                     10:00AM
                   </p>
                 </div>
 
-                <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center
-                bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                <span
+                  className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center
+                bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]"
+                >
                   <img src={videoIcon} alt="" />
                 </span>
               </div>
 
-              <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] 
+              <div
+                className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] 
               text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4
-              rounded-full">
+              rounded-full"
+              >
                 Consultation
               </div>
 
               <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
                 <img src={avatarIcon} alt="" />
-                <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700]
-                text-headingColor">Anil Piparaiya IITK </h4>
+                <h4
+                  className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700]
+                text-headingColor"
+                >
+                  Anil Piparaiya IITK{" "}
+                </h4>
               </div>
             </div>
           </div>
