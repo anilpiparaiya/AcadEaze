7e0ff3c3d05fa77e0f0a088edd56680de85c9c65
Anil Piparaiya <anilkumarpiparaiya@gmail.com>
2024-01-29 00:54:21 +0530

    minor fixes

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -16,8 +16,8 @@ const ServiceCard = ({ item, index }) => {
         <Link
           to="/mentors"
           className="w-[44px] h-[44px] rounded-full border border-solid 
-                  border-[#181A1E] mt-[30px] mx-auto flex items-center 
-                  justify-center group-hover:bg-primaryColor hover:border-none"
+          border-[#181A1E] mt-[30px] mx-auto flex items-center 
+          justify-center group hover:bg-primaryColor hover:border-none"
         >
           <BsArrowRight className="group-hover:text-white w-6 h-5" />
         </Link>
